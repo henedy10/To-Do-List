e26c39613f74493162f770ce62cfce4d19a7b3a2
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-05-10 23:31:05 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/accounts',[AccountController::class,'index'])->name('accounts.index');
