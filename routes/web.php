897b634d0bd1f89836112328b42af1d7fc38c109
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-05-11 16:03:47 +0300

    another commit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/',[AccountController::class,'index'])->name('accounts.index');
 Route::get('/accounts/{account}/edit',[AccountController::class,'edit'])->name('accounts.edit');
 Route::get('/accounts/create', [AccountController::class,'create'])->name('accounts.create');
