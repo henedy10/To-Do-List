7bdd85a92f84d78a00e80afec121537d255fe1f9
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-05-12 16:02:46 +0300

    another commit

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -9,27 +9,23 @@
 </head>
 <body>
     <div class="content_form d-flex justify-content-center align-items-center" style="height:100vh"  >
-        <form class="p-5" style="border: solid red 2px">
+        <form class="p-5" style="border: solid red 2px;width:25%">
             @csrf
             <div class="mb-3" >
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <label for="exampleInputPassword1" class="form-label">New_Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Confirm_Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
             <div class="mb-5">
-                <button type="submit" class="btn btn-primary mb-3" style="width: 100%">Login</button>
-                <span>Forget Password ? <a href="#">Click Here</a></span>
-            </div>
-            <div>
-                <span style=" display:block; text-align:center">Not a member yet? <a href="#">Sign Up</a></span>
+                <button type="submit" class="btn btn-primary mb-3" style="width: 100%">Update</button>
+                <a href="{{route('accounts.index')}}">Back</a>
             </div>
         </form>
     </div>
