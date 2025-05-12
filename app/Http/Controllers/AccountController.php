b4a3d3d3450b41e7f0f609c46993b9cad69b4389
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-05-12 18:36:44 +0300

    another commit

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationData;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class AccountController extends Controller
 {
-    public function index()  {
+    public function index(){
+        request()->validate([
+            'email'=>'required',
+            'password'=>'required',
+        ]);
         return view('accounts.index');
     }
     public function edit(){
@@ -15,7 +21,11 @@ class AccountController extends Controller
     public function create(){
         return view('accounts.create');
     }
-    public function show(){
-        return 'hello';
+    public function show($id){
+        // $email=request()->email;
+        // $password=request()->password;
+
+        // $user=User::where('email',$email)->first();
+        // return $id;
     }
 }
